901773007412fc3041ebc33628f1ea74fd1dc8be
quantlibdemo <quantlibdemo@outlook.com>
2019-07-03 20:26:52 +0000

    update code

---
+++ src/quantlib_test.cpp
@@ -0,0 +1,16 @@
+// wget https://bintray.com/quantlib/releases/download_file?file_path=QuantLib-1.15.tar.gz
+// tar -xf download_file\?file_path\=QuantLib-1.15.tar.gz
+// rm -rf download_file\?file_path\=QuantLib-1.15.tar.gz
+//
+// TO BUILD (note: takes a long time...):
+//
+// cd QuantLib-1.15
+// ./configure
+// make
+
+#include <ql/qldefines.hpp>
+
+int main()
+{
+    return 0;
+}
