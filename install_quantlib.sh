da1c8026e0408178c1a90d848ab6fd615be848d0
quantlibdemo <quantlibdemo@outlook.com>
2019-07-04 13:53:46 +0000

    update examples

--- install_quantlib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-wget https://bintray.com/quantlib/releases/download_file?file_path=QuantLib-1.15.tar.gz
-tar -xf download_file\?file_path\=QuantLib-1.15.tar.gz
-rm -rf download_file\?file_path\=QuantLib-1.15.tar.gz
-cd QuantLib-1.15
-./configure
-make
-make install
-ldconfig # this script should be run as root
--- src/boost_test.cpp
+++ src/boost_test.cpp
@@ -1,9 +1,13 @@
+// to run:
+//      g++ boost_test.cpp -o boost_test
+//      ./boost_test
+
 #include <iostream>
 #include <boost/array.hpp>
 
 using namespace std;
 int main(){
   boost::array<int, 4> arr = {{1,2,3,4}};
-  cout << "hi" << arr[0];
+  cout << "hi" << arr[0] << endl;
   return 0;
 }
